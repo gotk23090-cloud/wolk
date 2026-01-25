31219997052fa6d16f2d26bef86339ed42364129
gotk23090-cloud <gotk23.090@gmail.com>
2026-01-25 17:43:18 +0900

    Add files via upload

---
+++ users.js
@@ -0,0 +1,35 @@
+const users = [
+  { id: "1",
+    email: "gotk23.090@std.chiba-c.ed.jp",
+    password: "Fyhhmmw5",
+    name: "gotk23.090",
+    access:"A",
+  },
+  { id: "2",
+    email: "free@gmail.com",
+    password: "free",
+    name: "free",
+    access:"B"
+  },
+  {
+    id: "3",
+    name: "Admin",
+    email: "admin@gmail.com",
+    password: "1234",
+    access: "A"
+  },
+  {
+    id: "4",
+    name: "Vip",
+    email: "vip@gmail.com",
+    password: "1234",
+    access: "B"
+  },
+  {
+    id: "5",
+    name: "User",
+    email: "user@gmail.com",
+    password: "1234",
+    access: "NONE"
+  }
+]
